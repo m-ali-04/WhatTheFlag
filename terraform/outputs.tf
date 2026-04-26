5da1f9a0fb4238d2471687d718531c55aec5ab6e
Muhammad Ali <mxhammad8li@gmail.com>
2026-04-26 21:05:30 +0500

    Fix resource name in outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,9 @@
 output "instance_public_ip" {
   description = "Public IP address of the EC2 instance"
-  value       = aws_instance.app_server.public_ip
+  value       = aws_instance.target_node.public_ip
 }
 
 output "instance_id" {
   description = "ID of the EC2 instance"
-  value       = aws_instance.app_server.id
+  value       = aws_instance.target_node.id
 }
